6c9f37d6c6151bcb75d07b3cd6ee684062c5a7ed
Emma <e.villeneuve@outlook.com>
2019-08-26 22:11:07 +0200

    ML  project p59 to 64
    Data exploration, cleaning, splitting train and test sets, handling categorical data

--- ML_geron_1.py
+++ ML_geron_1.py
@@ -12,6 +12,8 @@ import tarfile
 from six.moves import urllib
 import pandas as pd
 import matplotlib.pyplot as plt
+from ML_tools import preprocessing
+from sklearn.preprocessing import Imputer, LabelEncoder, OneHotEncoder, LabelBinarizer
 
 # GET DATA
 # DOWNLOAD_ROOT = "https://github.com/ageron/handson-ml/tree/master/"
@@ -26,15 +28,66 @@ def load_housing_data(housing_path=HOUSING_PATH):
 
 
 # Explore data
-housing = load_housing_data()
-print(housing.head())
-print(housing.info())
+housing_dataset = load_housing_data()
+print(housing_dataset.head())
+print(housing_dataset.info())
 
-print(housing["ocean_proximity"].value_counts())
+print(housing_dataset["ocean_proximity"].value_counts())
 
 # stats for each variable
-print(housing.describe())
+print(housing_dataset.describe())
 
 # display histograms of variables
-housing.hist(bins=50, figsize=(20, 15))
-plt.show()
+housing_dataset.hist(bins=50, figsize=(20, 15))
+# plt.show()
+
+# CREATE A TEST SET
+housing_train, housing_test, tmp, tmp2 = preprocessing.split_data(housing_dataset, None, training_split_rate=0.80, display=True)
+
+# PREPARE THE DATA FOR ML ALGORITHMS (p.59)
+housing_labels = housing_train.loc[:, "median_house_value"].copy()
+housing = housing_train.drop(labels="median_house_value", axis=1)
+print(housing_labels.head())
+print(housing.info())
+
+# Data cleaning
+""" 3 options to deal with missing values
+- get rid of corresponding rows
+- get rid of the whole attribute (columns)
+- set the values to some value (zero, mean, median, etc)
+"""
+median_bedrooms = housing['total_bedrooms'].median()
+print(median_bedrooms)
+housing['total_bedrooms'].fillna(median_bedrooms, inplace=True)
+print(housing.info())
+# Data cleaning : other method
+imputer = Imputer(strategy="median")
+#   compute df with only numerical values
+housing_num = housing.drop('ocean_proximity', axis=1)
+# fit the imputer with the training data
+imputer.fit(housing_num)
+print(imputer.statistics_)
+print(housing_num.median().values)
+# apply the imputer to all the numerical values (result is a numpy array)
+X = imputer.transform(housing_num)
+housing_tr = pd.DataFrame(X, columns=housing_num.columns)
+
+# HANDLING TEXT AND CATEGORICAL ATTRIBUTES (p.62)
+# to convert text labels into numbers
+encoder = LabelEncoder()
+housing_cat = housing["ocean_proximity"]
+housing_cat_encoded = encoder.fit_transform(housing_cat)
+print(housing_cat_encoded)
+print(encoder.classes_)
+# One-hot encoding : binary classes from the encoded category vector, output is sparse matrix
+encoder = OneHotEncoder()
+housing_cat_1hot = encoder.fit_transform(housing_cat_encoded.reshape(-1, 1))
+print(housing_cat_1hot)
+# do integer encoding and binary encoding more quickly, output is a dense numpy array by default
+# unless you add sparse_output=True
+encoder = LabelBinarizer()
+housing_cat_1hot = encoder.fit_transform(housing_cat)
+print(housing_cat_1hot)
+
+# CUSTOM TRANSFORMERS (p.64)
+
